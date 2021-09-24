71889b84732468806f3bb8978dc6e1c86c6c43bc
Metsanpeitto <abraham@waldenberginc.com>
2021-09-24 05:43:54 +0200

    Fix style

--- main.rb
+++ main.rb
@@ -9,15 +9,15 @@ $alphabet = { '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D',
 $morse = '      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...
 '
 
-def word_decoder(_w)
-  result = _w
-  result = $alphabet[_w.to_sym] if _w != '/'
+def word_decoder(word)
+  result = word
+  result = $alphabet[word.to_sym] if word != '/'
   result
 end
 
-def message_decoder(m)
+def message_decoder(message)
   decoded_words = []
-  words_arr = m.split(' ')
+  words_arr = message.split(' ')
   words_arr.each do |word|
     decoded_word = word_decoder(word)
     decoded_words.push(decoded_word)
@@ -25,9 +25,9 @@ def message_decoder(m)
   decoded_words.join('')
 end
 
-def morse_decoder(m)
+def morse_decoder(message)
   decoded_messages = []
-  messages_arr = m.split('   ')
+  messages_arr = message.split('   ')
   messages_arr.each do |message|
     decoded_message = message_decoder(message)
     decoded_messages.push(decoded_message)
